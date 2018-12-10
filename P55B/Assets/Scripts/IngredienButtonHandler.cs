8888c172d51b1e981d9e1812d9204d97a78072f9
Mia Wölm <mia.wl095@googlemail.com>
2018-12-10 19:11:05 +0100

    added close button to ingredient screen

---
+++ P55B/Assets/Scripts/IngredienButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IngredienButtonHandler : MonoBehaviour {
+
+    public Button backButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        backButton.onClick.AddListener(BackToMain);
+    }
+
+    void BackToMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
