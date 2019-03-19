14915c54abf22d56a49fed1b50c5744c8c6cbaac
kiusah <44655147+kiusah@users.noreply.github.com>
2019-03-19 19:23:24 +0100

    Added events influencing ingredients spawnrate

--- P55B/Assets/Script/Planet/System/IngredientSpawner.cs
+++ P55B/Assets/Script/Planet/System/IngredientSpawner.cs
@@ -9,17 +9,20 @@ public class IngredientSpawner : MonoBehaviour {
 	public GameObject ingredient;
 	[Space]
 	[Header("Variables")]
-	public bool stopSpawning = false;
-    //public int spawnMaximum;
 	public float spawnTime;
 	public float spawnDelay;
 	public static int objectCounter;
-    public Vector3[] spawnPoints; 
+    private float timer = 0f;
     private Camera cam;
+    private Vector3[] spawnPoints; 
     private Vector3 spawnpoint1 = new Vector3();
     private Vector3 spawnpoint2 = new Vector3();
     private Vector3 spawnpoint3 = new Vector3();
     private Vector3 spawnpoint4 = new Vector3();
+    private Vector3 spawnpoint5 = new Vector3();
+    private Vector3 spawnpoint6 = new Vector3();
+    private Vector3 spawnpoint7 = new Vector3();
+    private Vector3 spawnpoint8 = new Vector3();
 
     #endregion
 
@@ -28,22 +31,32 @@ public class IngredientSpawner : MonoBehaviour {
     void Start ()
 	{
 		objectCounter = GameObject.FindGameObjectsWithTag("Moveable").Length;
-		InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
         cam = Camera.main;
-        spawnpoint1 = cam.ScreenToWorldPoint(new Vector3(0, 1.2f*Screen.height / 5, 15.5f));
-        spawnpoint2 = cam.ScreenToWorldPoint(new Vector3(0, 2*Screen.height / 5, 15.5f));
-        spawnpoint3 = cam.ScreenToWorldPoint(new Vector3(0, 3*Screen.height / 5, 15.5f));
-        spawnpoint4 = cam.ScreenToWorldPoint(new Vector3(0, 4*Screen.height / 5, 15.5f));
-        spawnPoints =  new Vector3[]{spawnpoint1, spawnpoint2 , spawnpoint3 , spawnpoint4 };
+        spawnpoint1 = cam.ScreenToWorldPoint(new Vector3(0, 1.4f*Screen.height / 5, 15.5f));
+        spawnpoint2 = cam.ScreenToWorldPoint(new Vector3(0, 1.8f * Screen.height / 5, 15.5f));
+        spawnpoint3 = cam.ScreenToWorldPoint(new Vector3(0, 2.1f*Screen.height / 5, 15.5f));
+        spawnpoint4 = cam.ScreenToWorldPoint(new Vector3(0, 3*Screen.height / 5, 15.5f));
+        spawnpoint5 = cam.ScreenToWorldPoint(new Vector3(0, 3.4f * Screen.height / 5, 15.5f));
+        spawnpoint6 = cam.ScreenToWorldPoint(new Vector3(0, 3.8f * Screen.height / 5, 15.5f));
+        spawnpoint7 = cam.ScreenToWorldPoint(new Vector3(0, 4.2f * Screen.height / 5, 15.5f));
+        spawnpoint8 = cam.ScreenToWorldPoint(new Vector3(0, 4.6f * Screen.height / 5, 15.5f));
+        spawnPoints =  new Vector3[]{spawnpoint1, spawnpoint2 , spawnpoint3 , spawnpoint4, spawnpoint5, spawnpoint6, spawnpoint7, spawnpoint8 };
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnDelay)
+        {
+            SpawnObject();
+            timer = 0f;
+        }
     }
 
 
-	public void SpawnObject()
+    public void SpawnObject()
 	{
-		if (stopSpawning)
-		{
-			CancelInvoke("SpawnObject");
-		}
+
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
         Instantiate(ingredient, spawnPoints[spawnPointIndex], new Quaternion(0.0f, 0.0f, 0.0f, 1.0f));
     }
---
+++ P55B/Assets/Script/Planet/System/SpawnerEvents.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerEvents : MonoBehaviour {
+
+    public IngredientSpawner spawner;
+    private bool eventInProgress = false;
+    private float timer;
+    private float eventOccurence;
+    private float eventType;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        StartEvent();
+	}
+
+    public void StartEvent()
+    {
+        if (eventInProgress == false)
+        {
+            eventOccurence = Random.value;
+            if (eventOccurence < .0002f)
+            {
+                eventInProgress = true;
+                eventType = Random.value;
+                if (eventType < .5f)
+                {
+                    StartCoroutine(SetLowSpawnrate());
+                    
+                }
+                else
+                {
+                    StartCoroutine(SetHighSpawnrate());
+                }
+            }
+        }
+    }
+
+    IEnumerator SetLowSpawnrate()
+    {        
+        spawner.spawnDelay = 5f;
+        Debug.Log("Low");
+        yield return new WaitForSecondsRealtime(30f);
+        spawner.spawnDelay = 1.3f;
+        eventInProgress = false;
+    }
+
+    IEnumerator SetHighSpawnrate()
+    {
+        spawner.spawnDelay = 0.5f;
+        Debug.Log("High");
+        yield return new WaitForSecondsRealtime(30f);
+        spawner.spawnDelay = 1.3f;
+        eventInProgress = false;
+
+    }
+
+
+}
