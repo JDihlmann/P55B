1bac77dbaf91cc979193458900fa7fd5f11fcb5f
Mia Wölm <mia.wl095@googlemail.com>
2019-03-31 17:40:40 +0200

    adds recipe image to drink slots

--- P55B/Assets/Script/UI/DrinkSlotsUI.cs
+++ P55B/Assets/Script/UI/DrinkSlotsUI.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrinkSlotsUI : MonoBehaviour {
 
     public GameObject drinkSlot;
 
     public Transform grid;
+
+    private int[] drinks = new int[] { 1, 0, 0, 1 };
 
     void Start()
     {
@@ -18,6 +21,10 @@ public class DrinkSlotsUI : MonoBehaviour {
         for (int i = 0; i < 4; i++)
         {
             GameObject holder = Instantiate(drinkSlot, grid);
+            Sprite sprite = Resources.Load<Sprite>("Sprites/" + IngredientManager.GetRecipes()[drinks[i]].Image);
+            holder.transform.GetChild(0).GetComponent<Image>().sprite = sprite;
+            holder.transform.GetChild(0).GetComponent<Image>().preserveAspect = true;
+            //GetComponent<Image>().sprite = sprite;
             //MachineStatsHolder holderScript = holder.GetComponent<MachineStatsHolder>();
             //holderScript.machineStat = machineStats[i];
         }
