983d7b546bbcd1425cf14bc6dc65669cca3d131f
Mia Wölm <mia.wl095@googlemail.com>
2019-04-02 19:14:19 +0200

    generates recipe drin slot modal window

---
+++ P55B/Assets/Script/UI/DrinkSlotModalWindowHolder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrinkSlotModalWindowHolder : MonoBehaviour {
+
+    public Transform grid;
+    public Button background;
+    public GameObject recipeButton;
+
+    private bool[] boughtRecipes = new bool[] {true, false, false, true };
+
+	// Use this for initialization
+	void Start () {
+        Button btn = background.GetComponent<Button>();
+        btn.onClick.AddListener(hideModal);
+
+        for(int i = 0; i < boughtRecipes.Length; i++)
+        {
+            if (boughtRecipes[i])
+            {
+                GameObject button = Instantiate(recipeButton, grid);
+                button.transform.GetChild(0).GetComponent<Text>().text = IngredientManager.GetRecipes()[i].Name;
+            }
+        }
+    }
+
+    public void hideModal()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
