f034575afff5df423259ae30a98c7174224e7afd
kiusah <44655147+kiusah@users.noreply.github.com>
2019-04-02 12:43:55 +0200

    Added Saving

---
+++ P55B/Assets/Script/Categrotize/SaveGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public class SaveGame : MonoBehaviour {
+
+    public static void Save()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        //string path = Application.persistentDataPath + "/ingredients.bla";
+        string path = "SaveGame.bla";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        SaveGameData data = new SaveGameData();
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SaveGameData Load()
+    {
+        //string path = Application.persistentDataPath + "/ingredients.bla";
+        string path = "SaveGame.bla";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            SaveGameData data = formatter.Deserialize(stream) as SaveGameData;
+            stream.Close();
+
+            return data;
+
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
---
+++ P55B/Assets/Script/Categrotize/SaveGameData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class SaveGameData {
+
+    public bool[] recipeUnlocks = new bool[5];
+    public int[] workerUnlocks = new int[4];
+    public int[] ingredientAmount = new int[4];
+    public int[] objectAmount = new int[5];
+    public int money = 0;
+    public int happiness = 0;
+
+    public SaveGameData()
+    {
+        
+        ingredientAmount = GameSystem.Instance.ingredientAmount;
+        money = GameSystem.Instance.money;
+        happiness = GameSystem.Instance.happiness;
+        objectAmount = GameSystem.Instance.objectAmount;
+        recipeUnlocks = GameSystem.Instance.recipeUnlocks;
+        workerUnlocks = GameSystem.Instance.workerUnlocks;
+        
+    }
+}
