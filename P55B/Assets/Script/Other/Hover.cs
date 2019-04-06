595b4c539be72f9b0d5d4a6dd6c1d9eecd044cd4
Jan-Niklas Dihlmann <m.dihlmann@googlemail.com>
2019-04-06 19:50:59 +0200

    Visual Overhaul V.1

---
+++ P55B/Assets/Script/Other/Hover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+	public float offset; 
+	public float lower_offset; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float delta = Mathf.Sin((Time.time + offset)*2); 
+		Vector3 pos = transform.position;
+
+		pos.y += (delta * lower_offset) * 0.0008f;
+		transform.position = pos; 
+	}
+}
---
+++ P55B/Assets/Script/Other/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float delta1 = Mathf.Sin(Time.time + 0); 
+		float delta2 = Mathf.Sin(Time.time + 1); 
+		float delta3 = Mathf.Sin(Time.time + 2); 
+		Vector3 angle = transform.eulerAngles;
+
+		angle.x += 0.5f ;
+		angle.y += 0.5f ;
+		angle.z += 0.5f  ;
+
+		//transform.eulerAngles = angle; 
+		
+		transform.Rotate ( new Vector3(delta1, delta2, delta3) * ( 100 * Time.deltaTime ) );
+	}
+}
+
