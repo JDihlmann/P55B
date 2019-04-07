1f006cf23eaee660b4f956f0795efa106fbfed00
Mia Wölm <mia.wl095@googlemail.com>
2019-04-07 16:05:31 +0200

    adds happiness skala

--- P55B/Assets/Script/Categrotize/GameStatsHolder.cs
+++ P55B/Assets/Script/Categrotize/GameStatsHolder.cs
@@ -7,9 +7,12 @@ public class GameStatsHolder : MonoBehaviour {
 
     [SerializeField]
     private Text money;
+    [SerializeField]
+    private Text happiness;
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         money.text = GameSystem.Instance.money.ToString();
+        happiness.text = GameSystem.Instance.happiness.ToString();
 	}
 }
